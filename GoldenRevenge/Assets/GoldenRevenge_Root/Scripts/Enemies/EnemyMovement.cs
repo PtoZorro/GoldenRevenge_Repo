fb6b505afc72875516708d3171db47a2ee32f4a5
JSF <javiersanchezferri12@gmail.com>
2024-12-13 12:00:52 +0100

    punytos de seguimiento enemigo

--- GoldenRevenge/Assets/GoldenRevenge_Root/Scripts/Enemies/EnemyMovement.cs
+++ GoldenRevenge/Assets/GoldenRevenge_Root/Scripts/Enemies/EnemyMovement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -12,7 +14,8 @@ public class EnemyMovement : MonoBehaviour
     [Header("References")]
     [SerializeField] EnemyCombat combat;
     Transform player;
-    
+    [SerializeField] List<Transform> patrolPoints;
+
     [Header("Stats")]
     [SerializeField] float chasingSpeed; // Velocidad a la que el enemigo persigue al Player
     [SerializeField] float patrolSpeed; // Velocidad a la que el enemigo patrulla
@@ -20,10 +23,13 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] float attackRadius; // Distancia a la cual el enemigo ataca al jugador
     [SerializeField] float detectRadius; // Distancia a la cual el enemigo detecta al jugador
     [SerializeField] float lostRadius; // Distancia a la cual el enemigo pierde de vista al jugador
+    [SerializeField] float pointReachThreshold; // Distancia desde un punto de patrulla a la cual el enemigo debe acercarse para cambiar a otro punto
 
     [Header("Conditional Values")]
     public bool isChasing; // Estado de persecución del jugador
     public bool isPatrolling; // Estado de patrullar
+    public bool returnPatrol; // Estado de volver a Patrullar
+    int currentPatrolPoint; // Punto que se setá persiguiendo actualmente
 
     void Start()
     {
@@ -43,8 +49,8 @@ public class EnemyMovement : MonoBehaviour
 
         // Sigue al jugador cuando se dan las condiciones
         FollowPlayer();
-        
-        // Patrulla cuando no hay detección
+
+        // Patrulla cuando no detecta jugador
         Patrolling();
 
         // Funciones cuando el Player está en el area de ataque
@@ -86,11 +92,11 @@ public class EnemyMovement : MonoBehaviour
         // Solo ejecutamos cuando esté en estado de perseguir
         if (!isChasing) return;
 
-        // Establecemos al jugador como el destino
-        agent.SetDestination(player.position);
-
         // Establecemos la velocidad de persecución
         agent.speed = chasingSpeed;
+
+        // Establecemos al jugador como el destino
+        agent.SetDestination(player.position);
     }
 
     void Patrolling()
@@ -98,11 +104,52 @@ public class EnemyMovement : MonoBehaviour
         // Solo ejecutamos cuando esté en estado de perseguir
         if (!isPatrolling) return;
 
-        // Establecemos al jugador como el destino
-        agent.SetDestination(transform.position); // ¡¡¡Provisional!!!
-
         // Establecemos la velocidad de persecución
         agent.speed = patrolSpeed;
+        
+        float distanceToPoint = Vector3.Distance(transform.position, patrolPoints[currentPatrolPoint].position);
+
+        if (distanceToPoint <= pointReachThreshold)
+        {
+            currentPatrolPoint = (currentPatrolPoint + 1) % patrolPoints.Count;
+        }
+
+        // Al inicio buscamos punto de patrulla más cercano
+        if (returnPatrol)
+        {
+            currentPatrolPoint = SearchClosestPatrol();
+            returnPatrol = false;
+        }
+
+        Vector3 targetPoint = patrolPoints[currentPatrolPoint].position;
+
+        // Establecemos el punto de destino inicial
+        agent.SetDestination(targetPoint);
+    }
+
+    // Función de búsqueda de punto de patrulla más cercano
+    int SearchClosestPatrol()
+    {
+        // Variables para almacenar el punto más cercano
+        int closestPoint = 0;
+        float closestDistance = Mathf.Infinity;
+
+        // Comparamos distancias entre enemigo y puntos
+        for (int currentPoint = 0; currentPoint < patrolPoints.Count; currentPoint++)
+        {
+            // Obtenemos a que distancia está el punto
+            float distanceToPoint = Vector3.Distance(transform.position, patrolPoints[currentPoint].position);
+
+            // Nos quedamos con el punto mas cercano
+            if (distanceToPoint < closestDistance)
+            {
+                closestDistance = distanceToPoint;
+                closestPoint = currentPoint;
+            }
+        }
+
+        // Devolvemos el punto más cercano
+        return closestPoint;
     }
 
     void InAttackArea()
@@ -110,11 +157,11 @@ public class EnemyMovement : MonoBehaviour
         // Solo ejecutamos en estado de ataque
         if (!combat.isAttacking) return;
 
-        // Establecemos al jugador como el destino
-        agent.SetDestination(player.position);
-
         // Negamos la velocidad para que no avance, pero puede seguir girando para mirar hacia el Player
         agent.speed = 0;
+
+        // Establecemos al jugador como el destino
+        agent.SetDestination(player.position);
     }
 
     void LookAtPlayer()
