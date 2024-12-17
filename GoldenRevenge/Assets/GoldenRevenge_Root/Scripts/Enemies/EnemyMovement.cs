4fbcba8ece070ebaeea79447830f72b6bdd975c9
PtoZorro <javiersanchezferri12@gmail.com>
2024-12-17 21:53:01 +0100

    paquete cargado

---
+++ GoldenRevenge/Assets/GoldenRevenge_Root/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,261 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Components
+    Rigidbody rb;
+    NavMeshAgent agent;
+
+    [Header("References")]
+    [SerializeField] EnemyCombat combat; // Script de control de combate
+    [SerializeField] List<Transform> patrolPoints; // Lista de puntos de patrullar
+    Transform player;
+
+    [Header("Stats")]
+    [SerializeField] float chasingSpeed; // Velocidad a la que el enemigo persigue al Jugador
+    [SerializeField] float patrolSpeed; // Velocidad a la que el enemigo patrulla
+    [SerializeField] float rotationSmooth; // Suavizado de la rotación hacia el Jugador
+    [SerializeField] float attackRadius; // Distancia a la cual el enemigo ataca al jugador
+    [SerializeField] float detectRadius; // Distancia a la cual el enemigo detecta al jugador
+    [SerializeField] float lostRadius; // Distancia a la cual el enemigo pierde de vista al jugador
+    [SerializeField] float pointReachThreshold; // Distancia desde un punto de patrulla a la cual el enemigo debe acercarse para cambiar a otro punto
+
+    [Header("Conditional Values")]
+    public bool isChasing; // Estado de persecución del jugador
+    public bool isPatrolling; // Estado de patrullar
+    public bool returnPatrol; // Estado de volver a Patrullar
+    public bool moveLocked; // Negación del movimiento
+    public bool rotationLocked; // Negación de rotación
+
+    // Posiciones
+    Vector3 target;
+    int currentPatrolPoint; // Punto que se setá persiguiendo actualmente
+
+    void Start()
+    {
+        // Obtenemos referencias
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player")?.transform;
+
+        // Valores de inicio
+        ChangeStates(true, false, false); // Comienza Patrullando
+    }
+
+    void Update()
+    {
+        // Acción que ejecutará el enemigo según la distancia del jugador
+        DetectPlayer();
+
+        // Sigue al jugador cuando se dan las condiciones
+        FollowPlayer();
+
+        // Mira hacia el Jugador cuando lo requiere
+        LookAtPlayer();
+
+        // Funciones durante estado de Patrullaje
+        Patrolling();
+
+        // Funciones cuando el Jugador está en el area de ataque
+        InAttackArea();
+    }
+
+    #region Detection&ChangeOfStates
+
+    // Acción que ejecutará el enemigo según la distancia del jugador
+    void DetectPlayer()
+    {
+        // Obtenemos a que distancia está el jugador
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Si el jugador no está en el area de detección
+        if (distanceToPlayer >= lostRadius && !isPatrolling)
+        {
+            ChangeStates(true, false, false); // Patrullando
+        }
+        else if (distanceToPlayer <= detectRadius && distanceToPlayer > attackRadius) // Si el jugador está en el area de detección
+        {
+            ChangeStates(false, true, false); // Persiguiendo
+        }
+        else if (distanceToPlayer <= attackRadius) // Si el jugador está en el area de ataque
+        {
+            ChangeStates(false, false, true); // Atacando
+        }
+    }
+
+    // Cambio de comportamiento del enemigo
+    void ChangeStates(bool patrolling, bool chasing, bool attacking)
+    {
+        isPatrolling = patrolling;
+        returnPatrol = patrolling; // Solo se activa al inicio del patrullaje
+        isChasing = chasing;
+        combat.isAttacking = attacking;
+    }
+
+    // Se llama para establecer el objetivo del enemigo
+    void SetTarget(Vector3 newTarget)
+    {
+        target = newTarget;
+        agent.SetDestination(target);
+    }
+
+    #endregion
+
+    #region Patrolling
+
+    // Funciones durante estado de Patrullaje
+    void Patrolling()
+    {
+        // Solo ejecutamos cuando esté en estado de patrullar y se pueda mover
+        if (!isPatrolling || moveLocked) return;
+
+        // Activamos la rotación de NavMesh y se desactiva la manual
+        agent.updateRotation = true; 
+
+        // Establecemos la velocidad de patrullar
+        agent.speed = patrolSpeed;
+
+        // Calculamos distancia hacia el punto que debe alcanzar
+        float distanceToPoint = Vector3.Distance(transform.position, patrolPoints[currentPatrolPoint].position);
+
+        // Si llega al punto, cambia al siguiente
+        if (distanceToPoint <= pointReachThreshold)
+        {
+            // Función para cambiar al siguiente punto de la lista de puntos 
+            currentPatrolPoint = (currentPatrolPoint + 1) % patrolPoints.Count;
+        }
+
+        // Al inicio buscamos punto de patrulla más cercano
+        if (returnPatrol)
+        {
+            currentPatrolPoint = SearchClosestPatrol();
+            returnPatrol = false;
+        }
+
+        // Establecemos el punto objetivo
+        Vector3 targetPoint = patrolPoints[currentPatrolPoint].position;
+
+        // Establecemos el jugador como Target
+        SetTarget(targetPoint);
+    }
+
+    // Función de búsqueda de punto de patrulla más cercano
+    int SearchClosestPatrol()
+    {
+        // Variables para almacenar el punto más cercano
+        int closestPoint = 0;
+        float closestDistance = Mathf.Infinity;
+
+        // Comparamos distancias entre enemigo y puntos
+        for (int currentPoint = 0; currentPoint < patrolPoints.Count; currentPoint++)
+        {
+            // Obtenemos a que distancia está el punto
+            float distanceToPoint = Vector3.Distance(transform.position, patrolPoints[currentPoint].position);
+
+            // Nos quedamos con el punto mas cercano
+            if (distanceToPoint < closestDistance)
+            {
+                closestDistance = distanceToPoint;
+                closestPoint = currentPoint;
+            }
+        }
+
+        // Devolvemos el punto más cercano
+        return closestPoint;
+    }
+
+    #endregion
+
+    #region PlayerInteraction
+
+    // Función de perseguir al jugador
+    void FollowPlayer()
+    {
+        // Solo ejecutamos cuando esté en estado de perseguir y se pueda mover
+        if (!isChasing || moveLocked) return;
+
+        // Activamos la rotación de NavMesh y se desactiva la manual
+        agent.updateRotation = true;
+
+        // Establecemos la velocidad de persecución
+        agent.speed = chasingSpeed;
+
+        // Establecemos el jugador como Target
+        SetTarget(player.position);
+    }
+
+    // Mira hacia el Jugador cuando lo requiere
+    void LookAtPlayer()
+    {
+        // Rotación manual hacia el target solo en estado de ataque y si no está negada
+        if (rotationLocked) return;
+
+        // Calculamos la dirección hacia el objetivo
+        Vector3 directionToTarget = target - transform.position;
+
+        // Mantener solo la dirección en el plano X-Z, eliminando cualquier inclinación en Y
+        directionToTarget.y = 0;
+
+        // Si la dirección es válida (evitar errores al normalizar un vector cero)
+        if (directionToTarget.sqrMagnitude > 0)
+        {
+            // Calculamos la rotación deseada solo en el eje Y
+            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+
+            // Suavizamos la rotación
+            Quaternion smoothedRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmooth * Time.deltaTime);
+
+            // Aplicamos la rotación suavizada
+            transform.rotation = smoothedRotation;
+        }
+    }
+
+    // Funciones cuando el Jugador está en el area de ataque
+    void InAttackArea()
+    {
+        // Solo ejecutamos en estado de ataque
+        if (!combat.isAttacking) return;
+
+        // Desactivamos la rotación de NavMesh para activar la manual
+        agent.updateRotation = false;
+
+        // Negamos la velocidad para que no avance, pero puede seguir girando para mirar hacia el Jugador
+        agent.speed = 0;
+
+        // Establecemos el jugador como Target
+        SetTarget(player.position);
+    }
+
+    #endregion
+
+    #region Depuration
+
+    // Los radios de detección se visualizan en el editor a modo de depuración
+    void OnDrawGizmosSelected()
+    {
+        // Radio de ataque
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+        // Radio de detección
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectRadius);
+
+        // Radio de pérdida
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lostRadius);
+
+        // Radio de alcance de puntos de Patrulla
+        for (int currentPoint = 0; currentPoint < patrolPoints.Count; currentPoint++)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(patrolPoints[currentPoint].position, pointReachThreshold);
+        }
+    }
+
+    #endregion
+}
--- GoldenRevenge/Assets/GoldenRevenge_Root/Scripts/Enemies/LockPointBehavior.cs
+++ GoldenRevenge/Assets/GoldenRevenge_Root/Scripts/Enemies/LockPointBehavior.cs
@@ -4,25 +4,22 @@ using UnityEngine;
 
 public class LockPointBehavior : MonoBehaviour
 {
+    // En este script manejamos la altura del "punto de marcado del enemigo"
+    // El punto de marcado es en el que la cámara se fijará automáticamente cuando fijemos un enemigo
+    // La altura del punto variará según lo cercano que esté el Jugador del enemigo,
+    // ya que así ajustamos desde que ángulo se verá al enemigo según la distancia, para que el Jugador no lo tape
+
     [Header("References")]
     Transform playerPos;
 
     [Header("Settings")]
-    [SerializeField] float riseHeight;
-    [SerializeField] float minDistance;
-    [SerializeField] float maxSpeed;
-    [SerializeField] float farPointY;
-
-    private float initialHeight;
+    [SerializeField] float farPointHeight; // Altura del punto de marcado cuando el jugador está lejos 
+    [SerializeField] float riseHeight; // Altura que se suma a la variable anterior según el Jugador se acerque
+    [SerializeField] float minDistance; // Distancia mínima del Jugador al enemigo a la que el punto comienza a subir
+    [SerializeField] float riseSpeed; // Velocidad a la que el punto se moverá
 
     void Start()
     {
-        // Movemos inicialmente al punto deseado
-        transform.localPosition = new Vector3(0, farPointY, 0);
-
-        // Guardamos la posición inicial del objeto
-        initialHeight = transform.position.y;
-
         // Encontramos al jugador en la escena 
         playerPos = GameObject.Find("Player")?.transform;
     }
@@ -39,19 +36,19 @@ public class LockPointBehavior : MonoBehaviour
             // Esta función nos da un valor entre 0 (fuera de rango o en el punto más cercano) y 1 (justo al lado del jugador)
             float t = 1 - Mathf.Clamp01(distanceToPlayer / minDistance);
 
-            // Calculamos la nueva posición vertical del objeto de manera proporcional
-            float newYPosition = Mathf.Lerp(initialHeight, initialHeight + riseHeight, t);
+            // Calculamos la nueva posición vertical del objeto interpolando entre la posición más baja y la más alta posible según t (cercania del Jugador entre 0-1)
+            float newYPosition = Mathf.Lerp(farPointHeight, farPointHeight + riseHeight, t);
 
             // Subimos el objeto con una velocidad limitada
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, newYPosition, transform.position.z), maxSpeed * Time.deltaTime);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, new Vector3(transform.localPosition.x, newYPosition, transform.localPosition.z), riseSpeed * Time.deltaTime);
         }
         else
         {
             // Recuperamos solo la altura inicial
-            Vector3 desiredHeight = new Vector3(transform.position.x, initialHeight, transform.position.z);
+            Vector3 desiredHeight = new Vector3(transform.localPosition.x, farPointHeight, transform.localPosition.z);
 
             // Si el jugador se aleja más de la distancia mínima, el objeto vuelve a su posición inicial
-            transform.position = Vector3.MoveTowards(transform.position, desiredHeight, maxSpeed * Time.deltaTime);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, desiredHeight, riseSpeed * Time.deltaTime);
         }
     }
 }
